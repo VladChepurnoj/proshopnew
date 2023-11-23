549c3411b070f2451aa5b259bd8b1bb87607493d
Vlad Chepurnyi <chepurnojv@gmail.com>
2023-11-23 12:19:12 +0200

    react setup, header and footer components, custom bootstrap theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-	return <div>Proshop</div>;
+	return (
+		<Fragment>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to proshop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</Fragment>
+	);
 };
 
 export default App;
+
