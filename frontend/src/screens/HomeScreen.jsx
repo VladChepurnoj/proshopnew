39abbaa4553f6440691e4c5e33a029063e7ce8b0
Vlad Chepurnyi <chepurnojv@gmail.com>
2023-11-28 14:31:39 +0200

    redux store and api slice, products api, get product details

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,29 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
-// import products from "../products";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(() => {
-		const fetchProduct = async () => {
-			const { data } = await axios.get("/api/products");
-			setProducts(data);
-		};
-
-		fetchProduct();
-	}, []);
+	const { data: products, isLoading, error } = useGetProductsQuery();
 
 	return (
 		<Fragment>
-			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{isLoading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<div>{error?.data?.message || error.error}</div>
+			) : (
+				<Fragment>
+					<h1>Latest Products</h1>
+					<Row>
+						{products.map((product) => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+				</Fragment>
+			)}
 		</Fragment>
 	);
 };
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,86 +1,93 @@
-import React, { Fragment, useState, useEffect } from "react";
-import axios from "axios";
+import React, { Fragment } from "react";
 import { useParams, Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-// import products from "../products";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
-	const [product, setProduct] = useState({});
-
 	const { id: productId } = useParams();
 
-	useEffect(() => {
-		const fetchProduct = async () => {
-			const { data } = await axios.get(`/api/products/${productId}`);
-			setProduct(data);
-		};
-
-		fetchProduct();
-	}, [productId]);
+	const {
+		data: product,
+		isLoading,
+		error,
+	} = useGetProductDetailsQuery(productId);
 
 	return (
 		<Fragment>
 			<Link className="btn btn-light my-3" to="/">
 				Go Back
 			</Link>
-			<Row>
-				<Col md={5}>
-					<Image src={product.image} alt={product.name} fluid></Image>
-				</Col>
 
-				<Col md={4}>
-					<ListGroup variant="flush">
-						<ListGroup.Item>
-							<h3>{product.name}</h3>
-						</ListGroup.Item>
+			{isLoading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<div>{error?.data?.message || error.error}</div>
+			) : (
+				<Fragment>
+					<Row>
+						<Col md={5}>
+							<Image src={product.image} alt={product.name} fluid></Image>
+						</Col>
 
-						<ListGroup.Item>
-							<Rating
-								value={product.rating}
-								text={`${product.numReviews} reviews`}></Rating>
-						</ListGroup.Item>
+						<Col md={4}>
+							<ListGroup variant="flush">
+								<ListGroup.Item>
+									<h3>{product.name}</h3>
+								</ListGroup.Item>
 
-						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-						<ListGroup.Item>Description: ${product.description}</ListGroup.Item>
-					</ListGroup>
-				</Col>
+								<ListGroup.Item>
+									<Rating
+										value={product.rating}
+										text={`${product.numReviews} reviews`}></Rating>
+								</ListGroup.Item>
 
-				<Col md={3}>
-					<Card>
-						<ListGroup variant="flush">
-							<ListGroup.Item>
-								<Row>
-									<Col>Price:</Col>
-									<Col>
-										<strong>${product.price}</strong>
-									</Col>
-								</Row>
-							</ListGroup.Item>
+								<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+								<ListGroup.Item>
+									Description: ${product.description}
+								</ListGroup.Item>
+							</ListGroup>
+						</Col>
 
-							<ListGroup.Item>
-								<Row>
-									<Col>Status:</Col>
-									<Col>
-										<strong>
-											{product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-										</strong>
-									</Col>
-								</Row>
-							</ListGroup.Item>
+						<Col md={3}>
+							<Card>
+								<ListGroup variant="flush">
+									<ListGroup.Item>
+										<Row>
+											<Col>Price:</Col>
+											<Col>
+												<strong>${product.price}</strong>
+											</Col>
+										</Row>
+									</ListGroup.Item>
 
-							<ListGroup.Item>
-								<Button
-									className="btn-block"
-									type="button"
-									disabled={product.countInStock === 0}>
-									Add To Card
-								</Button>
-							</ListGroup.Item>
-						</ListGroup>
-					</Card>
-				</Col>
-			</Row>
+									<ListGroup.Item>
+										<Row>
+											<Col>Status:</Col>
+											<Col>
+												<strong>
+													{product.countInStock > 0
+														? "In Stock"
+														: "Out Of Stock"}
+												</strong>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+
+									<ListGroup.Item>
+										<Button
+											className="btn-block"
+											type="button"
+											disabled={product.countInStock === 0}>
+											Add To Card
+										</Button>
+									</ListGroup.Item>
+								</ListGroup>
+							</Card>
+						</Col>
+					</Row>
+				</Fragment>
+			)}
 		</Fragment>
 	);
 };
