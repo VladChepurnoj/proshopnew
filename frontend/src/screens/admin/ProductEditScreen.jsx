70170cd78b6ece65f20c9480cdc53c1e971c5c89
Vlad Chepurnyi <chepurnojv@gmail.com>
2023-12-19 12:28:17 +0200

    upload product image, delete products

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -8,6 +8,7 @@ import { toast } from "react-toastify";
 import {
 	useUpdateProductMutation,
 	useGetProductDetailsQuery,
+	useUploadProductImageMutation,
 } from "../../slices/productsApiSlice";
 
 const ProductEditScreen = () => {
@@ -30,6 +31,9 @@ const ProductEditScreen = () => {
 
 	const [updateProduct, { isLoading: loadingUpdate }] =
 		useUpdateProductMutation();
+
+	const [uploadProductImage, { isLoading: loadingUpload }] =
+		useUploadProductImageMutation();
 
 	const navigate = useNavigate();
 
@@ -66,6 +70,18 @@ const ProductEditScreen = () => {
 		}
 	};
 
+	const uploadFileHandler = async (e) => {
+		const formData = new FormData();
+		formData.append("image", e.target.files[0]);
+		try {
+			const res = await uploadProductImage(formData).unwrap();
+			toast.success(res.message);
+			setImage(res.image);
+		} catch (err) {
+			toast.error(err?.data?.message || err.error);
+		}
+	};
+
 	return (
 		<Fragment>
 			<Link className="btn btn-light my-3" to="/admin/productlist">
@@ -96,7 +112,19 @@ const ProductEditScreen = () => {
 								value={price}
 								onChange={(e) => setPrice(e.target.value)}></Form.Control>
 						</Form.Group>
-						{/* image placeholder*/}
+
+						<Form.Group controlId="image" className="my-2">
+							<Form.Label>Image</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter image url"
+								value={image}
+								onChange={(e) => setImage(e.target.value)}></Form.Control>
+							<Form.Control
+								label="Choose File"
+								onChange={uploadFileHandler}
+								type="file"></Form.Control>
+						</Form.Group>
 
 						<Form.Group controlId="brand" className="my-2">
 							<Form.Label>Brand</Form.Label>
