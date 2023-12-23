610f66c5e6fdd4fe478367cfd50b91b8cbff1eeb
Vlad Chepurnyi <chepurnojv@gmail.com>
2023-12-23 11:49:39 +0200

    products pagination, paginated components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
+import { useParams } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-	const { data: products, isLoading, error } = useGetProductsQuery();
+	const { pageNumber } = useParams();
+
+	const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
 	return (
 		<Fragment>
@@ -20,12 +24,13 @@ const HomeScreen = () => {
 				<Fragment>
 					<h1>Latest Products</h1>
 					<Row>
-						{products.map((product) => (
+						{data.products.map((product) => (
 							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
 								<Product product={product} />
 							</Col>
 						))}
 					</Row>
+					<Paginate pages={data.pages} page={data.page}></Paginate>
 				</Fragment>
 			)}
 		</Fragment>
